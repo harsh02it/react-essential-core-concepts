59cea991ee245b833d82b40467be1a3cea569606
harsh02it <harshitsavani@gmail.com>
2024-07-11 19:49:54 -0400

    updated the Examples code to make a Tabs wrapper component

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,58 +1,8 @@
-import { EXAMPLES } from "../data.js";
-import { TabButton } from "./TabButton.jsx";
-
-import { useState } from "react";
-
-export default function Tabs() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic from above.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
+export default function Tabs({ children, buttons }) {
   return (
     <>
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === "components"}
-          onClick={() => handleSelect("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "jsx"}
-          onClick={() => handleSelect("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "props"}
-          onClick={() => handleSelect("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "state"}
-          onClick={() => handleSelect("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <menu>{buttons}</menu>
+      {children}
     </>
   );
 }
